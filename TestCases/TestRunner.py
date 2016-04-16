223f9731ef5ed23072dc5dea685884257d01ecc2
kmohamed <kmohamed@integrant.com>
2016-04-16 15:58:40 +0200

    testCI

--- TestCases/TestRunner.py
+++ TestCases/TestRunner.py
@@ -1,7 +1,7 @@
 import unittest
 import datetime as dt
 import sys
-sys.path.append("/Mokingjay_Olyve/TestCases/")
+sys.path.append("/Mokingjay_Olyve/")
 from TestCases.HomePageTest import HomePageTest
 from TestCases.PlaceOrderTest import PlaceOrderTest
 from Utilities import HTMLTestRunner
